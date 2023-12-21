21b7227c97c29aacd73a268796530847d9149d4f
Dalso <flskwhdhk@gmail.com>
2023-12-21 15:54:33 +0900

    실로폰앱 만들기 스타트

---
+++ android/app/src/main/kotlin/com/dalso/flutter_xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dalso.flutter_xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/xylophone_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
