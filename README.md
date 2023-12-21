51653ce98216824d42266784707552282514f0ac
Dalso <flskwhdhk@gmail.com>
2023-12-21 16:14:18 +0900

    실로폰앱 기본틀 만들기

--- README.md
+++ README.md
@@ -2,15 +2,4 @@
 
 실로폰 프로젝트 만들기
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Ios 에선 라이브러리 문제로 작동안하는 이슈가 있음
--- lib/xylophone_app.dart
+++ lib/xylophone_app.dart
@@ -1,10 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // 가로모드로 고정
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft
+    ]);
+
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('실로폰'),
+        ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          gunban('도', Colors.red, 8),
+          gunban('레', Colors.orange, 16),
+          gunban('미', Colors.yellow, 24),
+          gunban('파', Colors.green, 32),
+          gunban('솔', Colors.blue, 40),
+          gunban('라', Colors.lightBlueAccent, 48),
+          gunban('시', Colors.purple, 56),
+        ],
+      ),
+    );
+  }
+
+  Widget gunban(String text, Color color, double size) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: size),
+      child: Container(
+        width: 50,
+        height: double.infinity,
+        color: color,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
